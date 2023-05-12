cc2acb099026e579cf98ece8efd48d2cc3c2219c
by275 <by275@protonmail.com>
2023-05-12 20:51:41 +0900

    check aspect ratio when image mode is set to `SJVA Proxy URL`

--- site_util.py
+++ site_util.py
@@ -189,7 +189,14 @@ class SiteUtil:
                 #ret['poster_image_url'] = cls.discord_proxy_get_target_poster(image_url)
                 #if ret['poster_image_url'] is None:
                 if image_mode == '1':
-                    ret['poster_image_url'] = cls.process_image_mode('4', image_url, proxy_url=proxy_url)
+                    im = Image.open(BytesIO(cls.session.get(image_url).content))
+                    w, h = im.size
+                    if w > h:
+                        # landscape to poster
+                        ret['poster_image_url'] = cls.process_image_mode('4', image_url, proxy_url=proxy_url)
+                    else:
+                        # already poster
+                        ret['poster_image_url'] = cls.process_image_mode('1', image_url, proxy_url=proxy_url)
                 else:
                     ret['poster_image_url'] = cls.process_image_mode('5', ret['image_url']) #포스터이미지 url 본인 sjva
                     #if image_mode == '3': # 디스코드 url 모드일때만 포스터도 디스코드로
