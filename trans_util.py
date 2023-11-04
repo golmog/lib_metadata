67cb93e46fea4db1f35daa0a3db0acfe731633dc
by275 <by275@protonmail.com>
2023-11-05 01:57:33 +0900

    구글 WEB v2: 최대 1500자 라인 단위로 모아서 번역
    len(text) == 1900 근처에서 제한이 있어서 회피해야 함

--- trans_util.py
+++ trans_util.py
@@ -10,9 +10,29 @@ from .plugin import P
 logger = P.logger
 
 
+def splittext(text: str, limit: int = 1500):
+    splits, acc = [], 0
+    for line in text.splitlines():
+        cnt = len(line)
+        if acc + cnt > limit:
+            yield "\n".join(splits)
+            splits, acc = [], 0
+        acc += cnt
+        splits.append(line)
+    yield "\n".join(splits)
+
+
 class TransUtil:
     @classmethod
-    def trans_google_web2(cls, text: str, source: str = "ja", target: str = "ko"):
+    def trans_google_web2(cls, text: str, **kwargs):
+        try:
+            return "\n".join(cls.__trans_google_web2(t, **kwargs) for t in splittext(text))
+        except Exception:
+            logger.exception("구글 WEB v2를 이용해 번역 중 예외! 원문을 반환합니다:", text)
+            return text
+
+    @classmethod
+    def __trans_google_web2(cls, text: str, source: str = "ja", target: str = "ko"):
         url = "https://translate.google.com/translate_a/single"
         headers = {"User-Agent": "GoogleTranslate/6.27.0.08.415126308 (Linux; U; Android 7.1.2; PIXEL 2 XL)"}
         params = {
@@ -25,13 +45,8 @@ class TransUtil:
             "client": "at",
             "dt": ("t", "ld", "qca", "rm", "bd", "md", "ss", "ex", "sos"),
         }
-
-        try:
-            res = requests.get(url, params=params, headers=headers, timeout=30).json()
-            return "".join(sentences[0] for sentences in res[0][:-1])
-        except Exception:
-            logger.exception("구글 WEB v2를 이용해 번역 중 예외! 원문을 반환합니다:", text)
-            return text
+        res = requests.get(url, params=params, headers=headers, timeout=30).json()
+        return "".join(sentences[0] for sentences in res[0][:-1])
 
     @classmethod
     def __trans(cls, *args, **kwargs):
